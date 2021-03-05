998b8637625b6a64e4235807ee87298d98555f5a
JillaVinay <vinay.jilla786@gmail.com>
2021-03-05 11:06:11 +0530

    Added Employee.java file

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee{
+
+	private int eid;
+	private String ename;
+	private double esal;
+
+	Employee(int id,String name,double sal)
+	{
+		this.eid=id;
+		this.ename=name;
+		this.esal=sal;
+	}
+}
